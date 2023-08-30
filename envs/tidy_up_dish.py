c12b7029f496c4128d6791c42609ddb2b42a467e
LiHaoyang0616 <hyli1606@gmail.com>
2023-08-30 14:25:51 -0700

    move_env

--- envs/tidy_up_dish.py
+++ envs/tidy_up_dish.py
@@ -7,6 +7,14 @@ class TidyUpDish(BaseEnv):
         super().__init__()
 
     def _add_workspace(self):
+
+        # add the kitchen_env
+        builder = self._scene.create_actor_builder()
+        builder.add_visual_from_file(filename='./assets/kitchen/kitchen1/kitchen_tietu.dae')
+        builder.add_multiple_collisions_from_file(filename='./assets/outputs/kitchen1.obj')
+        kitchen_env = builder.build_kinematic(name='kitchen_env') # can not be affected by external forces
+        kitchen_env.set_pose(sapien.Pose(p=[-6.7, -3.4225, -1.0+0.23],q=[0.707, 0, 0, -0.707]))
+
         # add the wall
         builder = self._scene.create_actor_builder()
         builder.add_box_visual(half_size=[0.05, 3.0, 3.5])
@@ -24,7 +32,7 @@ class TidyUpDish(BaseEnv):
         builder.add_visual_from_file(filename='./assets/kitchen/sink/stainless_steel_sink.dae')
         builder.add_multiple_collisions_from_file(filename='./assets/kitchen/sink/stainless_steel_sink.obj')
         sink = builder.build(name='sink') # can not be affected by external forces
-        sink.set_pose(sapien.Pose(p=[-1.058, 0.019, -0.0096],q=[0.707, 0, 0, -0.707]))
+        sink.set_pose(sapien.Pose(p=[-1.058, 0.0, -0.0096],q=[0.707, 0, 0, -0.707]))
         pass
 
 
@@ -36,19 +44,13 @@ class TidyUpDish(BaseEnv):
         -"dishes",
         -""
         """
-        # add the kitchen_env
-        builder = self._scene.create_actor_builder()
-        builder.add_visual_from_file(filename='./assets/kitchen/kitchen1/kitchen_tietu.dae')
-        builder.add_multiple_collisions_from_file(filename='./assets/outputs/kitchen1.obj')
-        kitchen_env = builder.build_kinematic(name='kitchen_env') # can not be affected by external forces
-        kitchen_env.set_pose(sapien.Pose(p=[-6.7, -3.4, -1.0+0.23],q=[0.707, 0, 0, -0.707]))
 
         # add the kitchen_table
         builder = self._scene.create_actor_builder()
         builder.add_visual_from_file(filename='./assets/kitchen/kitchen_table/table_sink.dae')
         builder.add_multiple_collisions_from_file(filename='./assets/outputs/table_sink.obj')
         kitchen_table = builder.build_kinematic(name='kitchen_table') # can not be affected by external forces
-        kitchen_table.set_pose(sapien.Pose(p=[0.30, 0.91, -1.0],q=[0.707, 0, 0, -0.707]))
+        kitchen_table.set_pose(sapien.Pose(p=[0.30, 0.9, -1.0],q=[0.707, 0, 0, -0.707]))
 
         # add the dishes
         builder = self._scene.create_actor_builder()
@@ -58,7 +60,7 @@ class TidyUpDish(BaseEnv):
             name = "dishes_" + str(i)
             dishes = builder.build(name = name) # can not be affected by external forces
             # dishes.set_pose(sapien.Pose(p=[-1.179, 0.8+0.2*i, -0.231+0.5],q=[-0.291, 0, 0, 0.957]))
-            dishes.set_pose(sapien.Pose(p=[-1.057, 0.031, 0.006+0.1*i],q=[-0.291, 0, 0, 0.957]))
+            dishes.set_pose(sapien.Pose(p=[-1.057, 0.03075, 0.006+0.1*i],q=[-0.291, 0, 0, 0.957]))
 
         # add the photo
         builder = self._scene.create_actor_builder()
